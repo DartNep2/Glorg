40449ed0d2cb04c5538fee7abd0dc83442323605
DartNep2 <ivandyseiko@gmail.com>
2024-03-28 15:37:24 +0300

    Creating the Project

--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -1,4 +1,27 @@
 package bank;
 
 public class Card {
+    public int getCARD_NUMBER() {
+        return CARD_NUMBER;
+    }
+
+    public Card(int CARD_NUMBER, int pinCode) {
+        this.CARD_NUMBER = CARD_NUMBER;
+        this.pinCode = pinCode;
+    }
+
+    public void setCARD_NUMBER(int CARD_NUMBER) {
+        this.CARD_NUMBER = CARD_NUMBER;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    private int CARD_NUMBER;
+    private int pinCode;
 }
